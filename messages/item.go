aa4296cc58046cfc4653cc3a6f343922b0fec0cf
kettek <kts@kettek.net>
2025-02-06 20:55:00 -0800

    Add ItemType

--- messages/item.go
+++ messages/item.go
@@ -52,6 +52,153 @@ func (f ItemFlags) Read() bool {
 	return int32(f)&0x0020 != 0
 }
 
+type ItemType int16
+
+func (t ItemType) IsSpecial() bool {
+	return t >= 1 && t <= 49
+}
+
+func (t ItemType) IsMeleeWeapon() bool {
+	return t >= 100 && t <= 149
+}
+
+func (t ItemType) IsRangedWeapon() bool {
+	return t >= 150 && t <= 199
+}
+
+// IsAmmo returns if the type is a ammo. Note that IsRangedWeapon contains the ammo type.
+func (t ItemType) IsAmmo() bool {
+	return t == 159 || t == 165 || t == 170 // arrows, bolts, and bombs.
+}
+
+func (t ItemType) IsArmor() bool {
+	return t >= 250 && t <= 399
+}
+
+func (t ItemType) IsBodyArmor() bool {
+	return t >= 250 && t <= 257
+}
+
+func (t ItemType) IsShield() bool {
+	return t >= 260 && t <= 269
+}
+
+func (t ItemType) IsHeadwear() bool {
+	return t >= 270 && t <= 279
+}
+
+func (t ItemType) IsCloak() bool {
+	return t == 280 || t == 281
+}
+
+func (t ItemType) IsBoots() bool {
+	return t == 290 || t == 291
+}
+
+func (t ItemType) IsGloves() bool {
+	return t == 300 || t == 301 || t == 305
+}
+
+func (t ItemType) IsBracers() bool {
+	return t == 310 || t == 311
+}
+
+func (t ItemType) IsGirdle() bool {
+	return t == 321
+}
+
+func (t ItemType) IsAmulet() bool {
+	return t == 381
+}
+
+func (t ItemType) IsRing() bool {
+	return t == 390 || t == 391
+}
+
+func (t ItemType) IsSkillObject() bool {
+	return t >= 450 && t <= 459
+}
+
+func (t ItemType) IsFoodOrAlchemy() bool {
+	return t >= 600 && t <= 649
+}
+
+func (t ItemType) IsFood() bool {
+	return t == 601
+}
+
+func (t ItemType) IsDrink() bool {
+	return t == 611
+}
+
+func (t ItemType) IsFlesh() bool {
+	return t >= 620 && t <= 627
+}
+
+func (t ItemType) IsAlchemical() bool {
+	return t >= 628 && t <= 645
+}
+
+func (t ItemType) IsSpellCastingConsumable() bool {
+	return t >= 650 && t <= 699
+}
+
+func (t ItemType) IsPotion() bool {
+	return t == 651
+}
+
+func (t ItemType) IsBalmOrDust() bool {
+	return t == 652
+}
+
+func (t ItemType) IsFigurine() bool {
+	return t == 653
+}
+
+func (t ItemType) IsScroll() bool {
+	return t == 661
+}
+
+func (t ItemType) IsSpellCastingItem() bool {
+	return t >= 700 && t <= 749
+}
+
+func (t ItemType) IsRod() bool {
+	return t == 701
+}
+
+func (t ItemType) IsWand() bool {
+	return t == 711
+}
+
+func (t ItemType) IsStaff() bool {
+	return t == 712
+}
+
+func (t ItemType) IsHorn() bool {
+	return t == 721
+}
+
+func (t ItemType) IsKey() bool {
+	return t >= 800 && t <= 849
+}
+
+func (t ItemType) IsReadable() bool {
+	return t >= 1000 && t <= 1049
+}
+
+func (t ItemType) IsLightSource() bool {
+	return t >= 1100 && t <= 1149
+}
+
+func (t ItemType) IsValuables() bool {
+	return t >= 2000 && t <= 2049
+}
+
+func (t ItemType) IsMisc() bool {
+	return t >= 8000 && t <= 8999
+}
+
 type ItemObject struct {
 	Tag         int32
 	Flags       ItemFlags
@@ -63,7 +210,7 @@ type ItemObject struct {
 	Anim        int16
 	AnimSpeed   int8
 	Nrof        int32
-	Type        int16
+	Type        ItemType
 }
 
 func (o ItemObject) GetName() string {
@@ -114,7 +261,7 @@ func (m *MessageItem2) UnmarshalBinary(data []byte) error {
 			obj.TotalWeight = obj.Weight * obj.Nrof
 		}
 		offset += 4
-		obj.Type = int16(data[offset])<<8 | int16(data[offset+1])
+		obj.Type = ItemType(int16(data[offset])<<8 | int16(data[offset+1]))
 		offset += 2
 		m.Objects = append(m.Objects, obj)
 	}
